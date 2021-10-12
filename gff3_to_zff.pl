ce276bceb44e9ae61660609d0d507fd7760308f3
Ian Korf <ifkorf@gmail.com>
2021-10-11 21:30:08 -0700

    sorry

--- gff3_to_zff.pl
+++ gff3_to_zff.pl
@@ -3,8 +3,31 @@ use strict;
 use warnings 'FATAL' => 'all';
 
 my %c;
-while (<>) {
-	my ($chr, $m, $f, $beg, $end, $ph, $str, $fr, $grp) = split;
+open(my $gf, "genes.gff") or die;
+while (<$gf>) {	
+	next if /^#/;
+	my ($chr, $m, $f, $beg, $end, $ph, $str, $fr, $grp) = split(/\t/, $_);
 	next unless $f eq 'CDS';
 	my ($name) = $grp =~ /Parent=(\S+)/;
+	($beg, $end) = ($end, $beg) if $str eq '-';
+	push(@{$c{$chr}{$name}}, [$beg, $end]);
 }
+
+my @id;
+open(my $ff, "gunzip -c genome.fa.gz |") or die;
+while (<$ff>) {
+	if (/>(\S+)/) {
+		push @id, $1;
+	}
+}
+
+foreach my $chr (@id) {
+	print ">$chr\n";
+	foreach my $gene (keys %{$c{$chr}}) {
+		foreach my $exon (@{$c{$chr}{$gene}}) {
+			print "Exon $exon->[0] $exon->[1] $gene\n";
+		}
+	}
+}
+
+
